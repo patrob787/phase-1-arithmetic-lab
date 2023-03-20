d8021ec216ec02e18dbdc903c0293d53a13e9a13
Patrick Roberts <patrob22@gmail.com>
2023-03-19 23:19:06 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random() + 1);
+
+const num3 = 29;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(3, 6, 9, 12, 15, 20);
